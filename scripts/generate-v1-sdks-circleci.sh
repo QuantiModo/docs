3bd4480931323e86728ebcfad2fd650ec25af0e9
Mike P. Sinn <m@quantimodo.com>
2016-10-25 00:33:09 -0500

    /bin/sh ./git_push.sh quantimodo quantimodo-sdk-${i} "minor update"

--- scripts/generate-v1-sdks-circleci.sh
+++ scripts/generate-v1-sdks-circleci.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+git config --global user.name "Mike Sinn"
+git config --global user.email m@quantimodo.com
 
 echo "Clone swagger-codegen..."
 git clone https://github.com/swagger-api/swagger-codegen
@@ -17,9 +19,12 @@ echo "Generate v1 SDKs"
 for i in "akka-scala" "android" "async-scala" "CsharpDotNet2" "csharp" "dart" "dynamic-html" "flash" "go" "html" "inflector" "java" "jaxrs" "nodejs" "objc" "perl" "php" "python" "qt5cpp" "ruby" "scala" "scalatra" "silex" "sinatra" "spring-mvc" "swift" "tizen" "typescript-angular" "typescript-node"
 do
     echo "Generating $i SDK"
-    ./run-in-docker.sh generate -i api-docs-v1.json -l $i -o quantimodo_v1_skds/$i --invoker-package QuantiModo
+    ./run-in-docker.sh generate -i api-docs-v1.json -l ${i} -o quantimodo_v1_skds/$i --invoker-package QuantiModo
     echo "Replacing localhost with app.quantimo.do"
     find . -type f -exec sed -i 's/localhost/app.quantimo.do/g' {} +
+    cd quantimodo_v1_skds/${i}
+    /bin/sh ./git_push.sh quantimodo quantimodo-sdk-${i} "minor update"
+    cd ../..
     # Create SDK Zip
     #zip -r quantimodo_v1_skds/$i.zip quantimodo_v1_skds/$i
     # Remove SDK folder
