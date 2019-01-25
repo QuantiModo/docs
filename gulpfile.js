7c03cc01d2184cde2ba57c5e3ff5cb03b249b08f
Mike P. Sinn <m@quantimodo.com>
2019-01-25 11:19:20 -0600

    var swaggerJsonUrl = 'https://raw.githubusercontent.com/QuantiModo/docs/master/swagger/swagger.json';

--- gulpfile.js
+++ gulpfile.js
@@ -149,7 +149,7 @@ function executeCommand(command, callback) {
         if(callback){callback(err);}
     });
 }
-var swaggerJsonUrl = 'https://raw.githubusercontent.com/QuantiModo/docs/develop/swagger/swagger.json';
+var swaggerJsonUrl = 'https://raw.githubusercontent.com/QuantiModo/docs/master/swagger/swagger.json';
 //swaggerJsonUrl = 'https://utopia.quantimo.do:4443/api/docs/swagger/swagger.json';
 function clone(organization, repoName, destinationFolder, callback){
     var repoUrl = 'https://github.com/' + organization + '/' + repoName;
